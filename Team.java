2453f0e98d6118d5dffd8fdef29770def7defe1b
Jett Crowson <jettcrowson@gmail.com>
2018-03-12 10:42:14 -0600

    Create team class

---
+++ Team.java
@@ -0,0 +1,12 @@
+public class Team{
+    
+    private String name;
+    private double power;
+    private int seed;
+
+    public Team(String name, double power, int seed){
+        this.name = name;
+        this.power = power;
+        this.seed = seed;
+    }
+}
